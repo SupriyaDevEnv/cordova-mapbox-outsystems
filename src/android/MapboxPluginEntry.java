47041225a653e6f65162d0112d8822cb25a79bb1
SupriyaDevEnv <supriya.m@thedigifac.net>
2026-08-20 13:38:39 +0530

    Remove touch event logging from installTouchRouter
    Removed debug logging for touch events in installTouchRouter method.

--- src/android/MapboxPluginEntry.java
+++ src/android/MapboxPluginEntry.java
@@ -1972,11 +1972,6 @@ public class MapboxPluginEntry extends CordovaPlugin {
 
     private void installTouchRouter(View webViewView) {
         webViewView.setOnTouchListener((view, event) -> {
-                    Log.d("MAPBOX_TOUCH",
-            "Touch X=" + event.getX() +
-            " Y=" + event.getY() +
-            " inside=" +
-            isInsideTouchableRect(event.getX(), event.getY()));
             if (mapView == null || isInsideTouchableRect(event.getX(), event.getY())) {
                 return false;
             }
